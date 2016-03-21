984c1d3da34bad6754e65da8aa796681dded87dd
Joe Cannatti <joecannatti@gmail.com>
2016-03-20 20:59:28 -0500

    Write task to file

--- src/datastore.rs
+++ src/datastore.rs
@@ -1,6 +1,12 @@
 use std::io;
+use std::fs;
+use std::io::Write;
 
 pub fn store_event(name: String, details: Vec<String>) -> io::Result<()> {
-    println!("Saving {} and with details count {}", name, details.len());
+    let mut f = try!(fs::File::create("/tmp/data.csv"));
+    let mut row_data = details.to_vec();
+    row_data.insert(0, name);
+    let row_string = row_data.join(",");
+    try!(f.write_all(row_string.as_bytes()));
     Ok(())
 }
