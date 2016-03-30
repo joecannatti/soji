b39d062a4cbaa17f2051a44dd790c1732c8a152f
Joe Cannatti <joecannatti@gmail.com>
2016-03-29 20:51:52 -0500

    Crazy implementation of displaying how long a task has been active... Rust is pretty hard

---
+++ install.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash
+sudo cp target/debug/soji /usr/local/bin/soji
--- src/datastore.rs
+++ src/datastore.rs
@@ -1,3 +1,6 @@
+extern crate chrono;
+use self::chrono::*;
+
 use std::io;
 use std::fs;
 use std::fs::OpenOptions;
@@ -6,15 +9,17 @@ use std::io::BufReader;
 use std::io::BufRead;
 
 pub fn store_event(name: String, details: Vec<String>) -> io::Result<()> {
+    let now: DateTime<Local> = Local::now();
     let mut f = OpenOptions::new()
                 .read(true)
                 .write(true)
                 .create(true)
                 .append(true)
-                .open("/tmp/data.csv")
+                .open("/tmp/.soji_data.csv")
                 .expect("couldn't open data file");
     let mut row_data = details.to_vec();
     row_data.insert(0, name);
+    row_data.push(now.format("%Y-%m-%d %H:%M:%S %z").to_string());
     let row_string = row_data.join(",");
     try!(f.write_all(row_string.as_bytes()));
     try!(f.write_all("\n".as_bytes()));
@@ -22,14 +27,18 @@ pub fn store_event(name: String, details: Vec<String>) -> io::Result<()> {
 }
 
 pub fn current_task() -> Result<String, String>{
-    let f = fs::File::open("/tmp/data.csv").expect("read failed");
+    let f = fs::File::open("/tmp/.soji_data.csv").expect("read failed");
     let f = BufReader::new(f);
-    let mut event_line : String = "".to_string();
+    let mut status : String = "".to_string();
     for line in f.lines() {
         let line_copy = line.unwrap();
-        if line_copy[..5] == "start".to_string() {
-            event_line = line_copy.to_string().split(",").nth(1).expect("not found").to_string();
+        if &line_copy[..5] == "start" {
+            let mut components = line_copy.split(",");
+            let _ = components.nth(0);
+            let event_name = components.nth(0).expect("start event expected to have task name").to_string();
+            let start_time = components.nth(0).expect("start event expected to have start time").to_string();
+            status = format!("{} -- {:?} min", event_name, ((Local::now().timestamp() - chrono::datetime::DateTime::parse_from_str(&start_time, "%Y-%m-%d %H:%M:%S %z").expect("date not found").timestamp()) / 60));
         }
     }
-    Ok(event_line)
+    Ok(status)
 }
