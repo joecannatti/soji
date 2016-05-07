cc862f34d075da57f1201eaf7a35872153b21e5f
Joe Cannatti <joecannatti@gmail.com>
2016-05-07 12:35:44 -0500

    Remove .sh extensions from bin files

--- schedule_break.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-crontab -l | { cat; echo "`date +'%M %H %d %m *' --date '+25 min'` $SOJI_DIR/break.sh"; } | crontab -
--- schedule_notification.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-message=$1
-min_from_now=$2
-crontab -l | { cat; echo "`date +'%M %H %d %m *' --date \"+$min_from_now min\"` $SOJI_DIR/notify.sh '$message'"; } | crontab -
